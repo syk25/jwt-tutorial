04811cd45cb639e8055b67f8bc4c4dc4ae3aaa57
syk25 <syk2245@gmail.com>
2024-02-04 04:08:04 +0900

    refactor: 회원가입 기능 구현

--- src/main/java/syk25/jwttutorial/controller/JoinController.java
+++ src/main/java/syk25/jwttutorial/controller/JoinController.java
@@ -3,12 +3,9 @@ package syk25.jwttutorial.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import syk25.jwttutorial.dto.JoinDto;
 import syk25.jwttutorial.service.JoinService;
-
-import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequiredArgsConstructor
